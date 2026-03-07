5865fa9e8e67d24d102d639b217da18372a521bd
emigrete <teowelyczko@gmail.com>
2026-03-07 20:52:36 -0300

    trust proxy

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.set('trust proxy', 1);
 
 // ==========================================
 // MIDDLEWARES
